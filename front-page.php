c6953da2e6583ae825a72f2400c9bb9d221f32e2
Jose Guerra <jose@gcostudios.com>
2019-07-01 22:41:24 +0100

    updated markup

--- front-page.php
+++ front-page.php
@@ -1,7 +1,13 @@
 <?php get_header(); ?>
 
-<div>
+<div class="hero-treatment">
   <h1 class="page-title">Welcome to my awesome homepage</h1>
 </div>
+
+<main>
+  <section>
+    <p>Hello World</p>
+  </section>
+</main>
 
 <?php get_footer();
