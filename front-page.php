e3064779cf490a2189cfe0fabb1f8eecfd5f61d4
Jose Guerra <jose@gcostudios.com>
2019-12-05 21:38:59 +0000

    updated markup

--- front-page.php
+++ front-page.php
@@ -1,6 +1,14 @@
 <?php get_header(); ?>
 
 <main>
+  <section class="hero-treatment">
+    <div class="hero-inner">
+      <h1 class="page-title">Creative Solutions</h1>
+      <p>We are a London based design studio<br>
+      providing design and develoment solutions.</p>
+      <a href="#">Learn more</a>
+    </div>
+  </section>
   <section>
     <p>Hello World</p>
   </section>
