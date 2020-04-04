09e875259fbcfe3dd3d48af4b115f0446bfee6f3
Jose Guerra <jose@gcostudios.com>
2020-04-04 22:54:59 +0100

    enqueued main js script

--- functions.php
+++ functions.php
@@ -127,7 +127,9 @@ function gcostudios_scripts() {
 
 	wp_enqueue_script( 'gcostudios-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'gcostudios-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+  wp_enqueue_script( 'gcostudios-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+  
+  wp_enqueue_script('gcostudios-main-js', get_template_directory_uri() . '/js/main.js', array(), '20200404', true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
